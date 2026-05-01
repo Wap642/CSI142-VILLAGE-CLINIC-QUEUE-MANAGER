14db62d9d8a29ad4525094f5c6c7a17b2a7dc3b8
Olerato555 <202404142@ub.ac.bw>
2026-05-01 02:40:13 +0200

    Encapsulation added

--- src/com/clinic/queue/ClinicQueue.java
+++ src/com/clinic/queue/ClinicQueue.java
@@ -2,34 +2,33 @@
 package com.clinic.queue;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.comparator;
 
 public class ClinicQueue{
-   private ArrayList<Patient>queue=new ArrayList<>();
+    private Arraylist<Patient>queue= new ArrayList<>();
 
     public void addPatient(Patient k){
-        if  (k.getCondition().equals("Emergency")){
-            queue.add(0,k);
+        queue.addPatient(k);
+        // Always sort by priority after adding 
+        Collections.sort(queue,Comparator.comparingInt(Patient:getPriority));
+    }
+    public void viewPatients(){
+        if (queue.isEmpty()){
+            System.out.println("No patients in queue");
         }
-     else{
-         queue.add(k);
-     }
-}
-public void viewPatients(){
-    if  (queue.size()==0){
-        System.out.println("");
-    }
-    else {
-      for(int i =0 ;i<queue.size(); i++)
-    System.out.println((i +1)+". "+queue.get(i)); 
+        else{
+            for (int i = 0;i<queue.size();i++)
+           System.out.println(i +1)+ "." + queue.get(i);     
+        }
     }
 }
-
-public void servePatient(){
-    if  (queue.size()>0){
-        System.out.println("Serving: "+ queue.get(0));
+ public void servePatient(){
+    if (!queue.isEmpty()){
+        System.out.println("Serving:"+queue.get(0));
         queue.remove(0);
     }
-    else {
-        System.out.println("No patients");   
+    else{
+        System.out.println("No patients");
     }
-}
+ }
